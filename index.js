2cd038936af1462b879b0d7d8068925ffd1a6095
smarten <smarten.developer@gmail.com>
2020-09-06 14:58:22 -0700

    Basic functionality

---
+++ index.js
@@ -0,0 +1,137 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+const readmeContent = [
+  {
+    headingLevel: 1,
+    heading: "Title",
+  },
+  {
+    headingLevel: 2,
+    heading: "Description",
+  },
+  {
+    headingLevel: 2,
+    heading: "Installation",
+  },
+  {
+    headingLevel: 2,
+    heading: "Usage",
+  },
+  {
+    headingLevel: 2,
+    heading: "License",
+  },
+  {
+    headingLevel: 2,
+    heading: "Credits",
+  },
+  {
+    headingLevel: 2,
+    heading: "Contributing",
+  },
+  {
+    headingLevel: 2,
+    heading: "Tests",
+  },
+];
+
+const userPrompts = [
+  {
+    type: "input",
+    name: "username",
+    message: "What is your GitHub username?",
+  },
+  {
+    type: "confirm",
+    name: "includeTOC",
+    message: "Would you like to include a table of contents?",
+  },
+  {
+    type: "input",
+    name: "title",
+    message: "What is the title of your project?",
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "Write out the description for your ReadMe.",
+  },
+  {
+    type: "input",
+    name: "installation",
+    message: "Please describe how to install your application.",
+  },
+  {
+    type: "input",
+    name: "usage",
+    message: "Describe how to use your application.",
+  },
+  {
+    type: "input",
+    name: "credits",
+    message:
+      "List all of your collaborators. Include any noteworthy third-party assets or tutorials.",
+  },
+  {
+    type: "input",
+    name: "license",
+    message:
+      "Describe what other developers can and cannot do with your project.",
+  },
+  {
+    type: "input",
+    name: "contributing",
+    message: "Write out how other developers may contribute to your project.",
+  },
+  {
+    type: "input",
+    name: "tests",
+    message: "Provide examples for how to run your tests.",
+  },
+];
+
+inquirer.prompt(userPrompts).then((answers) => {
+  const answerArr = Object.values(answers);
+  console.log(answerArr);
+  for (let i = 2; i < answerArr.length; i++) {
+    readmeContent[i - 2].content = answerArr[i];
+  }
+  console.log(readmeContent);
+  fs.writeFile("./readme/README.md", "", (err) => {
+    if (err) {
+      throw err;
+    }
+    console.log("Successfully created readme file.");
+  });
+  writeToFile();
+});
+
+function writeToFile() {
+  readmeContent.forEach((item) => {
+    if (item.heading == "Installation") {
+      let tcContent = "## Table of Contents\n";
+      let tableOfContents = [];
+      readmeContent.forEach((item) => {
+        if (item.content) {
+          console.log(item);
+          tableOfContents.push(`[${item.heading}](#${item.heading})\n\n`);
+          console.log(tableOfContents);
+        }
+      });
+      tableOfContents.forEach((item) => {
+        console.log(tableOfContents, item);
+        tcContent = tcContent.concat(item);
+      });
+      fs.appendFileSync("./readme/README.md", tcContent);
+    }
+    let content = "";
+    const headingLevel = `${"#".repeat(item.headingLevel)} `;
+    const heading = `${item.heading}\n`;
+    if (item.content) {
+      content = content.concat(headingLevel, heading, `${item.content}\n`);
+    }
+    console.log(content);
+
+    fs.appendFileSync("./readme/README.md", content);
+  });
+}
---
+++ readme/README.md
@@ -0,0 +1,33 @@
+# Title
+Seanye
+## Description
+This is a desc
+## Table of Contents
+[Title](#Title)
+
+[Description](#Description)
+
+[Installation](#Installation)
+
+[Usage](#Usage)
+
+[License](#License)
+
+[Credits](#Credits)
+
+[Contributing](#Contributing)
+
+[Tests](#Tests)
+
+## Installation
+Weeeee
+## Usage
+Use it?
+## License
+Smarten
+## Credits
+Idk, whatever
+## Contributing
+DO a PR
+## Tests
+Test it
