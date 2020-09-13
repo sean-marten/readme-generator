8d167170ce44f9f9f67d2a4f587c84def5b650ac
smarten <smarten.developer@gmail.com>
2020-09-13 14:39:05 -0700

    full function, needs to be cleaned

--- index.js
+++ index.js
@@ -49,7 +49,12 @@ const userPrompts = [
   {
     type: "input",
     name: "repoName",
-    message: "What is your the name of the repository for this readme?",
+    message: "What is the name of the repository for this readme?",
+  },
+  {
+    type: "input",
+    name: "authToken",
+    message: "Please enter a valid Github Auth token.",
   },
   {
     type: "confirm",
@@ -61,77 +66,80 @@ const userPrompts = [
     name: "title",
     message: "What is the title of your project?",
   },
-  {
-    type: "editor",
-    name: "description",
-    message: "Write out the description for your ReadMe.",
-  },
-  {
-    type: "editor",
-    name: "installation",
-    message: "Please describe how to install your application.",
-  },
-  {
-    type: "editor",
-    name: "usage",
-    message: "Describe how to use your application.",
-  },
-  {
-    type: "editor",
-    name: "credits",
-    message:
-      "List all of your collaborators. Include any noteworthy third-party assets or tutorials.",
-  },
-  {
-    type: "editor",
-    name: "license",
-    message:
-      "Describe what other developers can and cannot do with your project.",
-  },
-  {
-    type: "editor",
-    name: "contributing",
-    message: "Write out how other developers may contribute to your project.",
-  },
-  {
-    type: "editor",
-    name: "tests",
-    message: "Provide examples for how to run your tests.",
-  },
+  // {
+  //   type: "editor",
+  //   name: "description",
+  //   message: "Write out the description for your ReadMe.",
+  // },
+  // {
+  //   type: "editor",
+  //   name: "installation",
+  //   message: "Please describe how to install your application.",
+  // },
+  // {
+  //   type: "editor",
+  //   name: "usage",
+  //   message: "Describe how to use your application.",
+  // },
+  // {
+  //   type: "editor",
+  //   name: "credits",
+  //   message:
+  //     "List all of your collaborators. Include any noteworthy third-party assets or tutorials.",
+  // },
+  // {
+  //   type: "editor",
+  //   name: "license",
+  //   message:
+  //     "Describe what other developers can and cannot do with your project.",
+  // },
+  // {
+  //   type: "editor",
+  //   name: "contributing",
+  //   message: "Write out how other developers may contribute to your project.",
+  // },
+  // {
+  //   type: "editor",
+  //   name: "tests",
+  //   message: "Provide examples for how to run your tests.",
+  // },
 ];
 
 inquirer.prompt(userPrompts).then((answers) => {
   const answerArr = Object.values(answers);
-  for (let i = 3; i < answerArr.length; i++) {
-    readmeContent[i - 3].content = answerArr[i];
+  for (let i = 4; i < answerArr.length; i++) {
+    readmeContent[i - 4].content = answerArr[i];
   }
   fs.writeFile("./readme/README.md", "", (err) => {
     if (err) {
       throw err;
     }
   });
-  getUserInfo(answerArr[2]);
-  writeToFile(answerArr[0], answerArr[1]);
+  getUserInfo(answerArr[0], answerArr[1], answerArr[2]);
 });
 
-async function getUserInfo(username) {
+async function getUserInfo(username, repoName, authToken) {
   var config = {
     headers: {
-      Authorization: "token a40a6741644ab2a36c2bf281de40f3190894d9d4",
+      Authorization: `token ${authToken}`,
     },
   };
   const queryUrl = `https://api.github.com/users/${username}`;
   try {
-    const { avatar_url, email } = await axios.get(queryUrl, config);
-    if (avatar_url && email) {
-      readmeContent[8].content = `![screenshot](${avatar_url})\nIf you have any questions, please contact me at ${email}`;
+    const { data } = await axios.get(queryUrl, config);
+    console.log(data);
+    if (data.avatar_url && data.email) {
+      readmeContent[8].content = `\nIf you have any questions, please contact me at ${data.email}\n\n![screenshot](${data.avatar_url})`;
     }
   } catch (e) {
     console.log(e);
   }
+
+  writeToFile(username, repoName);
 }
 
 function writeToFile(username, repoName) {
+  console.log(readmeContent);
   readmeContent.forEach((item) => {
     if (item.heading === "Installation") {
       let tcContent = "## Table of Contents\n";
@@ -157,11 +165,5 @@ function writeToFile(username, repoName) {
       content = content.concat(headingLevel, heading, `${item.content}\n`);
     }
     fs.appendFileSync("./readme/README.md", content);
-    if (item.heading === "Questions") {
-      fs.appendFileSync(
-        "./readme/README.md",
-        `https://avatars.githubusercontent.com/${username}.png`
-      );
-    }
   });
 }
