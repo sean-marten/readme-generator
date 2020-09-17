445284f716a0eacf01fb8008dcc2444dd6e7da92
smarten <smarten.developer@gmail.com>
2020-09-16 22:39:11 -0700

    final changes

--- index.js
+++ index.js
@@ -1,17 +1,12 @@
 const axios = require("axios");
 const fs = require("fs");
 const inquirer = require("inquirer");
-
 const { readmeContent, userPrompts } = require("./constants");
 
-init();
+promptUser();
 
-function init() {
-  promptUser();
-}
-
-function promptUser() {
-  inquirer
+async function promptUser() {
+  await inquirer
     .prompt({
       type: "confirm",
       name: "readme",
@@ -23,19 +18,25 @@ function promptUser() {
       if (!readme) {
         return;
       }
-      inquirer.prompt(userPrompts).then((answers) => {
-        const answerArr = Object.values(answers);
-        for (let i = 3; i < answerArr.length; i++) {
-          readmeContent[i - 3].content = answerArr[i];
-        }
-        fs.writeFile("./readme/README.md", "", (err) => {
-          if (err) {
-            console.log(`Failed to generate README.md file, ${err}`);
-          }
-        });
-        getUserInfo(answerArr[0], answerArr[1], answerArr[2]);
-      });
     });
+  await inquirer.prompt(userPrompts).then((answers) => {
+    const { username, repoName, authToken, ...content } = answers;
+    const listContent = Object.values(content);
+    for (let i = 0; i < listContent.length; i++) {
+      readmeContent[i].content = listContent[i];
+    }
+    fs.writeFile("./readme/README.md", "", (err) => {
+      if (err) {
+        console.log(`Failed to generate README.md file, ${err}`);
+      }
+    });
+    runStuff(username, repoName, authToken);
+  });
+}
+
+async function runStuff(username, repoName, authToken) {
+  await getUserInfo(username, repoName, authToken);
+  writeToFile(username, repoName);
 }
 
 async function getUserInfo(username, repoName, authToken) {
@@ -46,37 +47,28 @@ async function getUserInfo(username, repoName, authToken) {
   };
   const queryUrl = `https://api.github.com/users/${username}`;
   try {
-    const { data } = await axios.get(queryUrl, config);
-    if (data.avatar_url && data.email) {
-      readmeContent[8].content = `\nIf you have any questions, please contact me at ${data.email}\n\n![screenshot](${data.avatar_url})`;
+    const {
+      data: { avatar_url, email },
+    } = await axios.get(queryUrl, config);
+    if (avatar_url && email) {
+      readmeContent[
+        readmeContent.indexOf(
+          readmeContent.find((x) => x.heading === "Questions")
+        )
+      ].content = `\n![screenshot](${avatar_url})\n\nIf you have any questions, please contact me at ${email}!`;
     }
   } catch (err) {
     console.log(`Unable to retrieve Github user content, ${err}`);
   }
-
-  writeToFile(username, repoName);
 }
 
 function writeToFile(username, repoName) {
   readmeContent.forEach((item) => {
     if (item.heading === "Installation") {
-      let tcContent = "## Table of Contents\n";
-      let tableOfContents = [];
-      readmeContent.forEach((item) => {
-        if (item.heading === "Title") {
-          return;
-        }
-        if (item.content) {
-          tableOfContents.push(`[${item.heading}](#${item.heading})\n\n`);
-        }
-      });
-      tableOfContents.forEach((item) => {
-        tcContent = tcContent.concat(item);
-      });
-      fs.appendFileSync("./readme/README.md", tcContent);
+      writeTableOfContents();
     }
     if (item.heading === "Tests") {
-      const badge = `[![contributions welcome](https://img.shields.io/badge/contributions-welcome-brightgreen.svg?style=flat)](https://github.com/${username}/${repoName}/issues)\n`;
+      const badge = `\n[![contributions welcome](https://img.shields.io/badge/contributions-welcome-brightgreen.svg?style=flat)](https://github.com/${username}/${repoName}/issues)\n`;
       fs.appendFileSync("./readme/README.md", badge);
     }
     let content = "";
@@ -84,12 +76,29 @@ function writeToFile(username, repoName) {
       let heading = "";
       const headingLevel = `${"#".repeat(item.headingLevel)} `;
       if (item.heading === "Title") {
-        content = content.concat(headingLevel, `${item.content}\n`);
+        content = headingLevel + `${item.content}\n`;
       } else {
         heading = `${item.heading}\n`;
-        content = content.concat(headingLevel, heading, `${item.content}\n`);
+        content = headingLevel + heading + `${item.content}\n`;
       }
     }
     fs.appendFileSync("./readme/README.md", content);
   });
+}
+
+function writeTableOfContents() {
+  let tocContent = "## Table of Contents\n";
+  let tableOfContents = [];
+  readmeContent.forEach((item) => {
+    if (item.heading === "Title") {
+      return;
+    }
+    if (item.content) {
+      tableOfContents.push(`[${item.heading}](#${item.heading})\n\n`);
+    }
+  });
+  tableOfContents.forEach((item) => {
+    tocContent = tocContent.concat(item);
+  });
+  fs.appendFileSync("./readme/README.md", tocContent);
 }
